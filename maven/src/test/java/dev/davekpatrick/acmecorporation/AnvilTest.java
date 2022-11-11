e6f9a5bd43ad87e095a63b4eb1fb081d38ee4143
//dkp <58055667+davekpatrick@users.noreply.github.com>
2022-11-11 11:10:41 -0500

    Freature/publish package (#24)

--- maven/src/test/java/dev/davekpatrick/acmecorporation/AnvilTest.java
+++ maven/src/test/java/dev/davekpatrick/acmecorporation/AnvilTest.java
@@ -1,4 +1,4 @@
-package dev.davekpatrick;
+package dev.davekpatrick.acmecorporation;
 
 import static org.junit.Assert.assertTrue;
 
@@ -7,7 +7,7 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AnvilTest 
 {
     /**
      * Rigorous Test :-)
